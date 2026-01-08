d0edc3f8090a1954ac5f6519a702a993df9c0383
joefedna <joe@fednaresearch.com>
2026-01-08 19:43:44 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,15 +11,15 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Fedna Research',
+    template: '%s | Fedna Research',
   },
-  description: 'This is my portfolio.',
+  description: 'This Is Not the Website.',
   openGraph: {
-    title: 'My Portfolio',
-    description: 'This is my portfolio.',
+    title: 'Not the Website.',
+    description: 'This Is Not the Website.',
     url: baseUrl,
-    siteName: 'My Portfolio',
+    siteName: 'Not the Website.',
     locale: 'en_US',
     type: 'website',
   },
