26c996243b99b67f495faf64a2fa4976a56ce004
joefedna <joe@fednaresearch.com>
2026-01-08 19:33:01 +0530

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -17,20 +17,19 @@ function ArrowIcon() {
 
 export default function Footer() {
   return (
-    <footer className="mb-16">
+<footer className="mb-16">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://fednaresearch.com/"
+            href="https://fednaresearch.com"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">visit website</p>
           </a>
         </li>
-      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
@@ -42,7 +41,6 @@ export default function Footer() {
             <p className="ml-2 h-7">demo</p>
           </a>
         </li>
-      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
